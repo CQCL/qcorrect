3186f4bb3c16b204a4b86d8f17a23aa88d48a0a1
Henry Semenenko <henry.semenenko@quantinuum.com>
2025-09-24 16:00:24 +0100

    Add more tests for qubit allocation

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -7,10 +7,14 @@ import qcorrect as qct
 
 
 def test_qubit_allocation():
-    @guppy
+    @guppy.comptime
     def main() -> None:
-        q = phys.qubit()
-        phys.measure(q)
+        q0 = phys.qubit()
+        q1 = phys.qubit()
+        phys.h(q0)
+        phys.cx(q0, q1)
+        phys.measure(q0)
+        phys.measure(q1)
 
     hugr = main.compile()
 
@@ -29,5 +33,37 @@ def test_qubit_array_allocation():
     hugr = main.compile()
 
     qct.allocate(hugr, qct.LinearAllocation(block_size=5))
+
+    check_hugr(hugr.to_bytes())
+
+
+def test_op_loop():
+    @guppy.comptime
+    def main() -> None:
+        q0 = phys.qubit()
+        for _ in range(2):
+            phys.h(q0)
+        phys.measure(q0)
+
+    hugr = main.compile()
+
+    qct.allocate(hugr, qct.LinearAllocation(block_size=5))
+
+    check_hugr(hugr.to_bytes())
+
+
+def test_mid_circuit_measurement():
+    @guppy.comptime
+    def main() -> None:
+        q0 = phys.qubit()
+        for _ in range(5):
+            anc = phys.qubit()
+            phys.cx(q0, anc)
+            phys.measure(anc)
+        phys.measure(q0)
+
+    hugr = main.compile()
+
+    qct.allocate(hugr, qct.LinearAllocation(block_size=2))
 
     check_hugr(hugr.to_bytes())
